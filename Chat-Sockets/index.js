ad1b22afee9d7ecb2f52670cb654061dc6f1d475
Richard Johansson <88727174+Zastq@users.noreply.github.com>
2021-12-09 14:08:50 +0100

    Socket IO.
    Homework.
    Done 5 of 6.
    -Shows who's online.
    -Add "user is typing".
    -Broadcast a message when someone connects and disconnects.

--- Chat-Sockets/index.js
+++ Chat-Sockets/index.js
@@ -1,25 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const {
   Server
 } = require("socket.io");
 const io = new Server(server);
 
+let counter=0;
+const userMap = new Map();
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+//socket.emit skickar endast till användaren som kallade på det.
+//io.emit skickar till alla.
+
+
 function onConnection(socket){
   
   const userName = socket.handshake.query.userName;
-  console.log(userName + ': user connected');
 
-  io.emit("chat message", userName + " - User have Connected.");
+  console.log(userName + ": user connected");
+  counter++;
+  io.emit("userCount", counter)
+
+  userMap.set(socket.id , userName );
+  // socket.broadcast.emit("userPerson", userMap.get(socket.id) )
+  io.emit("userList", Object.fromEntries(userMap))
+
+  socket.broadcast.emit("chat message", userName + " - User have Connected.");
+
   socket.on('disconnect', () => {
     console.log(userName + ": user disconnected");
-    io.emit("chat message", userName + " - User have Disconnected")
+    counter--;
+    io.emit("userCount", counter);
+
+    userMap.delete(socket.id)
+    io.emit("userList", Object.fromEntries(userMap))
+    
+    socket.broadcast.emit("chat message", userName + " - User have Disconnected")
   });
 }
 
@@ -41,11 +62,17 @@ io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log("Message - " + msg);
 
-    socket.broadcast.emit('chat message', msg); // Skickar till alla förutom avsendaren.
+    socket.broadcast.emit('chat message', msg); // Skickar chat meddelendet till alla förutom avsendaren.
   });
+  socket.on("typing", (userName) =>
+  {
+    userName = socket.handshake.query.userName;
+    console.log(userName);
+    socket.broadcast.emit("typing", userName)
+  })
 });
 
 
-server.listen(3001, () => {
-  console.log('listening on *:3001');
+server.listen(3002, () => {
+  console.log('listening on *:3002');
 });
